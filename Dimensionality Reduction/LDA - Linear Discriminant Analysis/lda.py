c7bb976593feb4784b944b043eb2a10a3ef556cf
avinashreddy147 <36698460+avinashreddy147@users.noreply.github.com>
2018-02-21 17:59:35 +0530

    Update lda.py

--- Dimensionality Reduction/LDA - Linear Discriminant Analysis/lda.py
+++ Dimensionality Reduction/LDA - Linear Discriminant Analysis/lda.py
@@ -18,7 +18,7 @@ X_test = sc.transform(X_test)
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 lda = LDA(n_components = 2) 
-X_train = lda.fit_transform(X_train, y_train) #LDA is Supervised so include y_train
+X_train = lda.fit_transform(X_train, y_train) 
 X_test = lda.transform(X_test)
 
 from sklearn.linear_model import LogisticRegression
